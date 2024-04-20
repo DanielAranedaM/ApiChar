b6ef027c0c9b8aa25ba40153633e29a537ab3c81
unknown <Cetecom@SJAPCA03LC0618.duoc.acad>
2024-04-20 11:18:05 -0400

    producto

---
+++ Clientes.API/Controllers/ProductoController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Clientes.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Clientes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private static IList<Producto> lista = new List<Producto>();
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return lista;
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(int id)
+        {
+            Producto resultado = lista.FirstOrDefault(x => x.id == id);
+            return resultado;
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] Producto value)
+        {
+            lista.Add(value);
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Producto value)
+        {
+            Producto selection = lista.FirstOrDefault(x => x.id == id);
+            //Va a buscar la posición del id que encontro
+            lista[lista.IndexOf(selection)] = value;
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Producto selection = lista.FirstOrDefault(x => x.id == id);
+            lista.Remove(selection);
+        }
+    }
+}
